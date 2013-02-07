0c046fca920a01ee4022eae64eeb6f01a4dfd7d3
powange <powange@gmail.com>
2013-02-07 04:10:36 +0100

    initialisation

---
+++ inc.os_navigateur.php
@@ -0,0 +1,603 @@
+<?php
+$lvc_OS['Win95']= 'Win 95';
+$lvc_OS['Win98']= 'Win98';
+$lvc_OS['Win98']= 'Win 9x';
+$lvc_OS['Win98']= 'Win9x';
+$lvc_OS['WinNT']= 'Win NT';
+$lvc_OS['WinNT']= 'WinNT4.0';
+$lvc_OS['WinMe']= 'Win Me';
+$lvc_OS['WinXP']= 'Win XP';
+$lvc_OS['Win2000'] = 'Win 2000';
+$lvc_OS['Linux']= 'Linux';
+$lvc_OS['Mac PPC'] = 'Mac PPC';
+$lvc_OS['PPC Mac'] = 'PPC Mac';
+$lvc_OS['SunOS']= 'SunOS';
+
+
+$lvc_agent['AOL'] = 'AOL Explorer;';
+$lvc_agent['Internet Explorer'] = 'IE;';
+$lvc_agent['Netscape']= 'NS;';
+$lvc_agent['Opera'] = 'OP;';
+$lvc_agent['Firefox']= 'Firefox';
+$lvc_agent['Firebird']= 'Firebird';
+$lvc_agent['AppleWebKit']= 'AppleWebKit';
+$lvc_agent['Safari']= 'Safari';
+$lvc_agent['Konqueror'] = 'Konqueror';
+$lvc_agent['Mozilla'] = 'Mozilla';
+
+$lvc_other_agt['Lynx']= 'Lynx - Linux';
+$lvc_other_agt['WWWOFFLE']= 'WWWOFFLE - Linux';
+$lvc_other_agt['Konqueror'] = 'Konqueror - Linux';
+
+$lvc_agent_versions['AOL'] = Array(
+	'9.0'
+	);
+
+$lvc_agent_versions['NS'] = Array(
+	'4.8',
+	'4.76',
+	'4.75',
+	'4.74',
+	'4.73',
+	'4.72',
+	'4.71',
+	'4.7',
+	'4.61',
+	'4.6',
+	'4.51',
+	'4.5',
+	'4.08',
+	'4.07',
+	'4.06',
+	'4.05',
+	'4.04',
+	'4.03'
+	);
+$lvc_agent_versions['NS6'] = Array(
+	'8.1',
+	'8.0.1',
+	'7.2',
+	'7.1',
+	'7.02',
+	'7.01',
+	'7.0',
+	'7.1',
+	'6.2.3',
+	'6.2.2',
+	'6.2',
+	'6.1',
+	'6.0',
+	'6.1',
+	'm14',
+	'm17',
+	'm18'
+	);
+$lvc_agent_versions['Firebird'] = Array(
+	'0.7',
+	);
+$lvc_agent_versions['Konqueror'] = Array(
+	'3.2',
+	);
+$lvc_agent_versions['Mozilla'] = Array(
+	'rv:1.7.10',
+	'1.7.10',
+	'rv:1.7.8',
+	'1.7.8',
+	'1.7.3',
+	'1.7',
+	'1.4',
+	);
+$lvc_agent_versions['AppleWebKit'] = Array(
+	'1.7.5',
+	'1.7.3',
+	'1.6',
+	'1.0.2',
+	);
+$lvc_agent_versions['Safari'] = Array(
+	'530.19',
+	'525.27.1',
+	'419.3',
+	'417.9.3',
+	'417.9.2',
+	'85.5',
+	'85.6',
+	'85.8',
+	'85.8.1',
+	'417.8',
+	'416.12',
+	'412',
+	'312',
+	'125.12',
+	'125.1',
+	'125.9',
+	'125.8',
+	);
+$lvc_agent_versions['Camino'] = Array(
+	'0.8.4',
+	'0.8.3',
+	'0.8.2',
+	'0.8.1',
+	'0.8int',
+	);
+
+$lvc_agent_os['IE'] = Array(
+	'Windows 95' => 'Win95',
+	'Win32'=> 'Win95',
+	'Win 9x 4.90'=> 'WinMe',
+	'Windows 98' => 'Win98',
+	'Windows NT 5.0' => 'Win2000',
+	'Windows NT 5.1' => 'WinXP',
+	'Windows NT 5.2' => 'Windows Server 2003',
+	'Windows NT 6.0' => 'WinVista',
+	'Windows NT 6.1' => 'WinSeven',
+	'Windows NT' => 'WinNT',
+	'Mac PowerPC'=> 'Mac PPC',
+	'Mac PPC'=> 'Mac PPC',
+	'SunOS'=> 'SunOS',
+	);
+$lvc_agent_os['NS'] = Array(
+	'Win95'=> 'Win95',
+	'Win 9x 4.90'=> 'WinMe',
+	'Win98'=> 'Win98',
+	'WinNT'=> 'WinNT',
+	'Windows NT 5.0' => 'Win2000',
+	'Windows NT 5.1' => 'WinXP',
+	'Windows NT 5.2' => 'Windows Server 2003',
+	'Windows NT 6.0' => 'WinVista',
+	'Windows NT 6.1' => 'WinSeven',
+	'Windows NT' => 'WinNT',
+	'Linux'=> 'Linux',
+	'SunOS'=> 'SunOS',
+	'PPC'=> 'Mac PPC',
+	'FreeBSD'=> 'FreeBSD',
+	'AIX'=> 'AIX',
+	'IRIX'=> 'IRIX',
+	'HP-UX'=> 'HP-UX',
+	'OS/2'=> 'OS/2',
+	'NetBSD' => 'NetBSD'
+	);
+$lvc_agent_os['NS6'] = Array(
+	'Win95'=> 'Win95',
+	'Win 9x 4.90'=> 'WinMe',
+	'Win98'=> 'Win98',
+	'WinNT'=> 'WinNT',
+	'Windows NT 5.0' => 'Win2000',
+	'Windows NT 5.1' => 'WinXP',
+	'Windows NT 5.2' => 'Windows Server 2003',
+	'Windows NT 6.0' => 'WinVista',
+	'Windows NT 6.1' => 'WinSeven',
+	'Windows NT' => 'WinNT',
+	'Linux'=> 'Linux',
+	'SunOS'=> 'SunOS',
+	'PPC'=> 'Mac PPC',
+	'FreeBSD'=> 'FreeBSD',
+	'AIX'=> 'AIX',
+	'IRIX'=> 'IRIX',
+	'HP-UX'=> 'HP-UX',
+	'OS/2'=> 'OS/2',
+	'NetBSD' => 'NetBSD'
+	);
+$lvc_agent_os['OP'] = Array(
+	'Windows 95' => 'Win95',
+	'Windows 98' => 'Win98',
+	'Windows 2000'=> 'Win2000',
+	'Win 9x 4.90'=> 'WinMe',
+	'Windows NT 5.0' => 'Win2000',
+	'Windows NT 5.1' => 'WinXP',
+	'Windows NT 5.2' => 'Windows Server 2003',
+	'Windows NT 6.0' => 'WinVista',
+	'Windows NT 6.1' => 'WinSeven',
+	'Windows NT' => 'WinNT',
+	'Linux'=> 'Linux'
+	);
+$lvc_agent_os['Firefox'] = Array(
+	'Windows 95' => 'Win95',
+	'Windows 98' => 'Win98',
+	'Win98' => 'Win98',
+	'Windows 2000'=> 'Win2000',
+	'Win 9x 4.90'=> 'WinMe',
+	'Windows NT 5.0' => 'Win2000',
+	'Windows NT 5.1' => 'WinXP',
+	'Windows NT 5.2' => 'Windows Server 2003',
+	'Windows NT 6.0' => 'WinVista',
+	'Windows NT 6.1' => 'WinSeven',
+	'Windows NT' => 'WinNT',
+	'WinNT4.0' => 'WinNT',
+	'Linux'=> 'Linux',
+	'Mac OS X' => 'Mac OS X',
+	'FreeBSD' => 'FreeBSD',
+	'SunOS'=> 'SunOS',
+	);
+$lvc_agent_os['Mozilla'] = Array(
+	'Windows 95' => 'Win95',
+	'Windows 98' => 'Win98',
+	'Win98' => 'Win98',
+	'Windows 2000'=> 'Win2000',
+	'Win 9x 4.90'=> 'WinMe',
+	'Windows NT 5.0' => 'Win2000',
+	'Windows NT 5.1' => 'WinXP',
+	'Windows NT 5.2' => 'Windows Server 2003',
+	'Windows NT 6.0' => 'WinVista',
+	'Windows NT 6.1' => 'WinSeven',
+	'Windows NT' => 'WinNT',
+	'Linux'=> 'Linux',
+	'Mac OS X' => 'Mac OS X',
+	'FreeBSD' => 'FreeBSD',
+	'SunOS'=> 'SunOS',
+	);
+$lvc_agent_os['Firebird'] = Array(
+	'Windows 95' => 'Win95',
+	'Windows 98' => 'Win98',
+	'Windows 2000'=> 'Win2000',
+	'Win 9x 4.90'=> 'WinMe',
+	'Windows NT 5.0' => 'Win2000',
+	'Windows NT 5.1' => 'WinXP',
+	'Windows NT 5.2' => 'Windows Server 2003',
+	'Windows NT 6.0' => 'WinVista',
+	'Windows NT 6.1' => 'WinSeven',
+	'Windows NT' => 'WinNT',
+	'Linux'=> 'Linux',
+	'Mac OS X' => 'Mac OS X',
+	'SunOS'=> 'SunOS',
+	);
+$lvc_agent_os['Safari'] = Array(
+	'Mac OS X' => 'Mac OS X',
+	);
+$lvc_agent_os['Konqueror'] = Array(
+	'Linux'=> 'Linux',
+	);
+$lvc_agent_os['Camino'] = Array(
+	'Mac OS X' => 'Mac OS X',
+	'PPC Mac OS X Mach-O' => 'Mac OS X',
+	);
+//Mozilla/4.0 (compatible; grub-client-2.6.0)
+//Mozilla/5.0 (compatible; Konqueror/3.2; Linux) KHTML/3.2.3 (like Gecko)
+//Mozilla/5.0 (compatible; Konqueror/3.2; Linux) (KHTML, like Gecko)
+//Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.5) Gecko/20031007 Firebird/0.7
+//Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.2) Gecko/20040804 Netscape/7.2 (ax)
+//Mozilla/5.0 (Windows; U; Windows NT 6.1; fr; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3
+//Mozilla/5.0 (Windows; U; Windows NT 6.1; fr; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2
+//Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Win64; x64; Trident/4.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; Tablet PC 2.0)
+//Mozilla/5.0 (Windows; U; Win98; fr-FR; rv:1.7.10)
+//Mozilla/5.0 (Windows; U; Win98; fr-FR; rv:1.7.10)
+//Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; fr-fr) AppleWebKit/530.19.2 (KHTML, like Gecko) Version/4.0.2 Safari/530.19
+//Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; zh-tw) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10
+//psbot/0.1 ( http://www.picsearch.com/bot.html)
+//Opera/9.80 (Windows NT 6.1; U; fr) Presto/2.5.22 Version/10.51
+//InsurancoBot( http://www.insuranco.de/bot.html)
+//Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) Speedy Spider (http://www.entireweb.com/about/search_tech/speedy_spider/)
+// ------------------------------------------------------------------------- //
+function analyse_agent($agt)
+{
+	global $lvc_agent_versions;
+	global $lvc_agent_versions_2;
+	global $lvc_agent_os;
+	global $lvc_other_agt;
+	$lvc_agent_max_length = 300;
+	
+	$agt = strtr($agt, '+', ' ');
+	
+	// AOL Explorer
+	if(	strpos($agt, 'AOL') !== FALSE )
+	{
+		$new_agt = 'AOL Explorer';
+		$agt = strtr($agt, '_', ' ');
+		for ($cnt = 0, $ok = false; $cnt < sizeof($lvc_agent_versions['AOL']) && !$ok; $cnt++)
+		{
+			if ($ok = strpos($agt, $lvc_agent_versions['AOL'][$cnt]))
+			{
+				$new_agt .= '-'.$lvc_agent_versions['AOL'][$cnt];
+				for (@reset($lvc_agent_os['AOL']), $ok = false; (list($key, $value) = @each($lvc_agent_os['AOL'])) && !$ok;)
+				{
+					if ($ok = ereg($key, $agt))
+						$new_agt .= '  '.$value;
+				}
+			}
+		}
+		if (!$ok) $new_agt = $agt;
+	}
+	// Internet Explorer
+	elseif(	strpos($agt, 'MSIE') !== FALSE && strpos($agt, 'Opera' ) === FALSE )
+	{
+		$new_agt = 'IE';
+		$new_agt .= '-'.preg_replace('`^.*MSIE ([0-9]*(\.[0-9]*)*).*$`si','$1',$agt);
+		
+		for (@reset($lvc_agent_os['IE']), $ok = false; (list($key, $value) = @each($lvc_agent_os['IE'])) && !$ok;)
+		{
+			if ($ok = ereg($key, $agt))
+				$new_agt .= '  '.$value;
+		}
+	}
+	// Opera
+	elseif(	strpos($agt, 'Opera') !== FALSE )
+	{
+		$new_agt = 'Opera';
+		$new_agt .= '-'.preg_replace('`^.*Version/([0-9]*(\.[0-9]*)*).*$`si','$1',$agt);
+		
+		for (@reset($lvc_agent_os['OP']), $ok = false; (list($key, $value) = @each($lvc_agent_os['OP'])) && !$ok;)
+		{
+			if ($ok = ereg($key, $agt))
+				$new_agt .= '  '.$value;
+		}
+	}
+	// Netscape 4.x
+	elseif(	strpos($agt, 'Mozilla/4.') !== FALSE )
+	{
+		$new_agt = 'NS';
+		for ($cnt = 0, $ok = false; $cnt < sizeof($lvc_agent_versions['NS']) && !$ok; $cnt++)
+		{
+			if ($ok = strpos($agt, $lvc_agent_versions['NS'][$cnt]))
+			{
+				$new_agt .= '-'.$lvc_agent_versions['NS'][$cnt];
+				for (@reset($lvc_agent_os['NS']), $ok = false; (list($key, $value) = @each($lvc_agent_os['NS'])) && !$ok;)
+				{
+					if ($ok = ereg($key, $agt))
+						$new_agt .= '  '.$value;
+				}
+			}
+		}
+		if (!$ok) $new_agt = $agt;
+	}
+	// Safari
+	elseif(	strpos($agt, 'Safari') !== FALSE )
+	{
+		$new_agt = 'Safari';
+		$new_agt .= '-'.preg_replace('`^.*Version/([0-9]*(\.[0-9]*)*).*$`si','$1',$agt);
+		
+		for (@reset($lvc_agent_os['Safari']), $ok = false; (list($key, $value) = @each($lvc_agent_os['Safari'])) && !$ok;)
+		{
+			if ($ok = ereg($key, $agt))
+			$new_agt .= '  '.$value;
+		}
+	}
+	// Firefox
+	elseif(	strpos($agt, 'Firefox') !== FALSE )
+	{
+		$new_agt = 'Firefox';
+		$new_agt .= '-'.preg_replace('`^.*Firefox/([0-9]*(\.[0-9]*)*).*$`si','$1',$agt);
+		
+		for (@reset($lvc_agent_os['Firefox']); (list($key, $value) = @each($lvc_agent_os['Firefox'])) && !$ok;)
+		{
+			if ($ok = ereg($key, $agt))
+				$new_agt .= '  '.$value;
+		}
+	}
+	// NS 6
+	elseif(	strpos($agt, 'Netscape') !== FALSE && strpos($agt, 'Konqueror') === FALSE )
+	{
+		$new_agt = 'Netscape';
+		for ($cnt = 0, $ok = false; $cnt < sizeof($lvc_agent_versions['NS6']) && !$ok; $cnt++)
+		{
+			if ($ok = strpos($agt, $lvc_agent_versions['NS6'][$cnt]))
+			{
+				$new_agt .= '-'.$lvc_agent_versions['NS6'][$cnt];
+				for (@reset($lvc_agent_os['NS']), $ok = false; (list($key, $value) = @each($lvc_agent_os['NS'])) && !$ok;)
+				{
+					if ($ok = ereg($key, $agt))
+						$new_agt .= '  '.$value;
+				}
+			}
+		}
+		if (!$ok) $new_agt = $agt;
+	}
+	// MOZILLA
+	elseif(	strpos($agt, 'Gecko') !== FALSE )
+	{
+		$new_agt = 'Mozilla';
+		for ($cnt = 0, $ok = false; $cnt < sizeof($lvc_agent_versions['Mozilla']) && !$ok; $cnt++)
+		{
+			if ($ok = strpos($agt, $lvc_agent_versions['Mozilla'][$cnt]))
+			{
+				$new_agt .= '-'.$lvc_agent_versions['Mozilla'][$cnt];
+				for (@reset($lvc_agent_os['Mozilla']), $ok = false; (list($key, $value) = @each($lvc_agent_os['Mozilla'])) && !$ok;)
+				{
+					if ($ok = ereg($key, $agt))
+						$new_agt .= '  '.$value;
+				}
+			}
+		}
+		if (!$ok) $new_agt = $agt;
+	}
+	// others
+	else
+	{
+		$new_agt = $agt;
+		for (@reset($lvc_other_agt), $ok = false; (list($key, $value) = @each($lvc_other_agt)) && !$ok;)
+		{
+			if ($ok = strpos($agt, $key))
+			$new_agt = $value;
+		}
+	}
+	
+	$new_agt = strip_tags($new_agt);
+	
+	//===================================================
+	if(	$new_agt=='Mediapartners-Google/2.1')
+		$new_agt='<span class="robot">Mediapartners</span>';
+		
+	//===================================================
+	elseif(	strpos($new_agt, 'Googlebot/2.1') )
+		$new_agt='<span class="robot">Robot Googlebot 2.1</span>';
+		
+	//===================================================
+	elseif(	strpos($new_agt, 'VoilaBot BETA 1.2') )
+		$new_agt='<span class="robot">Robot VoilaBot BETA 1.2</span>';
+		
+	//===================================================
+	elseif(	strpos($new_agt, 'Teoma')
+		||	$new_agt=='Mozilla/2.0 (compatible; Ask Jeeves/Teoma; http://sp.ask.com/docs/about/tech_crawling.html)')
+		$new_agt=='<span class="robot">Teoma Robot</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='Mozilla/4.0 compatible ZyBorg/1.0 (wn-12.zyborg@looksmart.net; http://www.WISEnutbot.com)'
+		||	$new_agt=='Mozilla/4.0 compatible ZyBorg/1.0 (wn-14.zyborg@looksmart.net; http://www.WISEnutbot.com)'
+		||	$new_agt=='Mozilla/4.0 compatible ZyBorg/1.0 Dead Link Checker (wn.dlc@looksmart.net; http://www.WISEnutbot.com)')
+		$new_agt='<span class="robot">Robot Looksmart v1.0</span>';
+
+
+	//===================================================
+	elseif(	$new_agt=='Mozilla/4.0 (compatible; grub-client-2.6.1)'
+		||	$new_agt=='Mozilla/4.0 (compatible; grub-client-2.6.0)')
+		$new_agt='<span class="robot">Robot Looksmart</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='Mozilla/4.0 (compatible; Cerberian Drtrs Version-3.2-Build-0)')
+		$new_agt='<span class="robot">Robot Cerberian</span>';
+		
+	//===================================================
+
+	elseif(	$new_agt=='Mozilla/5.0 (compatible; BecomeBot/2.0.3; http://www.become.com/webmasters.html)')
+		$new_agt='<span class="robot">Become Robot 2.0.3</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='Mozilla/5.0 (compatible; BecomeBot/3.0; MSIE 6.0 compatible; http://www.become.com/site owners.html)')
+		$new_agt='<span class="robot">BecomeBot 3.0</span>';
+		
+	//===================================================
+
+	elseif(	$new_agt=='Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)')
+		$new_agt='<span class="robot">Robot Yahoo</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='Mozilla/5.0 (compatible; Yahoo! Slurp China; http://misc.yahoo.com.cn/help.html)')
+		$new_agt='<span class="robot">Robot Yahoo China</span>';
+		
+	//===================================================
+	elseif(	strpos($new_agt, 'Exabot/3.0') )
+		$new_agt='<span class="robot">Exabot 3.0</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='TurnitinBot/2.0 http://www.turnitin.com/robot/crawlerinfo.html')
+		$new_agt='<span class="robot">Robot Turnitin 2.0</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='ia_archiver')
+		$new_agt='<span class="robot">Alexa Robot (ia_archiver)</span>';
+
+	//===================================================
+	elseif(	$new_agt=='appie 1.1 (www.walhello.com)')
+		$new_agt='<span class="robot">Robot Walhello </span>';
+		
+	//===================================================
+	elseif(	$new_agt=='ConveraCrawler/0.4')
+		$new_agt='<span class="robot">Robot Convera 0.4</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='ConveraCrawler/0.8 ( http://www.authoritativeweb.com/crawl)')
+		$new_agt='<span class="robot">Robot Convera 0.8</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='ConveraCrawler/0.9d ( http://www.authoritativeweb.com/crawl)')
+		$new_agt='<span class="robot">Robot Convera 0.9</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='Iltrovatore-Setaccio/1.2 (It-bot; http://www.iltrovatore.it/bot.html; info@iltrovatore.it)')
+		$new_agt='<span class="robot">Iltrovatore Robot Italien</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='Seekbot/1.0 (http://www.seekbot.net/bot.html) HTTPFetcher/0.3')
+		$new_agt='<span class="robot">SEEKBOT Robot 3.0</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='NutchCVS/0.06-dev (Nutch; http://www.nutch.org/docs/en/bot.html; nutch-agent@lists.sourceforge.net)')
+		$new_agt='<span class="robot">Robot Looksmart</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='NutchCVS/0.05 (Nutch; http://www.nutch.org/docs/en/bot.html; nutch-agent@lists.sourceforge.net)')
+		$new_agt='<span class="robot">Robot Looksmart</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='sherlock/1.0')
+		$new_agt=='<span class="robot">Robot Sherlock v1.0</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='Wotbox/0.7-alpha (bot@wotbox.com; http://www.wotbox.com)')
+		$new_agt='<span class="robot">Robot Wotbox/0.7-alpha</span>';
+		
+	// Robot Almaden bc23 ===================================================
+	elseif(	$new_agt=='http://www.almaden.ibm.com/cs/crawler [bc23]')
+		$new_agt='<span class="robot">Robot Almaden bc23</span>';
+		
+	// Robot IRLbot v1.0 ===================================================
+	elseif(	$new_agt=='IRLbot/1.0 ( http://irl.cs.tamu.edu/crawler)')
+		$new_agt='<span class="robot">Robot IRLbot v1.0</span>';
+		
+	// Robot MSN 0.3 ===================================================
+	elseif(	$new_agt=='msnbot/0.3 (+http://search.msn.com/msnbot.htm)')
+		$new_agt='<span class="robot">Robot MSN 0.3</span>';
+		
+	// Robot MSN 0.9 ==================================================
+	elseif(	$new_agt=='msnbot/0.9 ( http://search.msn.com/msnbot.htm)')
+		$new_agt='<span class="robot">Robot MSN 0.9</span>';
+		
+	// Robot MSN 1.0 ===================================================
+	elseif(	strpos($new_agt, 'msnbot/1.0') )
+		$new_agt='<span class="robot">Robot MSN 1.0</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='aipbot/1.0 (aipbot; http://www.aipbot.com; aipbot@aipbot.com)')
+	
+		$new_agt='<span class="robot">Aipbot</span>';
+		
+	//===================================================
+	elseif(	strpos($new_agt, 'Gigabot/2.0') )
+		$new_agt='<span class="robot">Robot Gigabot</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='psbot/0.1 ( http://www.picsearch.com/bot.html)')
+	
+		$new_agt='<span class="robot">Robot Picsearch</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='InsurancoBot( http://www.insuranco.de/bot.html)')
+	
+		$new_agt='<span class="robot">InsurancoBot</span>';
+		
+	//==================================================
+	elseif(	$new_agt=='IP*Works! V5 HTTP/S Component - by /n software - www.nsoftware.com')
+	
+		$new_agt='<span class=\"robot ip_protect\">IP prot&eacute;g&eacute;e</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='FAST MetaWeb Crawler (helpdesk at fastsearch dot com)')
+	
+		$new_agt='<span class="robot">FastSearch Bot</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='e-SocietyRobot(http://www.yama.info.waseda.ac.jp/~yamana/es/)')
+	
+		$new_agt='<span class="robot">e-SocietyRobot</span>';
+		
+	//===================================================
+	elseif(	strpos($new_agt, 'Baiduspider') )
+		$new_agt='<span class="robot">Robot Baiduspider</span>';
+		
+	//===================================================
+	elseif(	strpos($new_agt, 'DotBot/1.1') )
+		$new_agt='<span class="robot">Robot DotBot 1.1</span>';
+		
+	//===================================================
+	elseif(	strpos($new_agt, 'BIGLOTRON') )
+		$new_agt='<span class="robot">Robot Biglotron</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='SBIder/0.8-dev (SBIder; http://www.sitesell.com/sbider.html; http://support.sitesell.com/contact-support.html)')
+	
+		$new_agt='<span class="robot">Robot SBIder 0.8</span>';
+		
+	//===================================================
+	elseif(	$new_agt=='Mozilla/5.0 (Twiceler-0.9 http://www.cuil.com/twiceler/robot.html)')
+	
+		$new_agt='<span class="robot">Robot Cuil.com</span>';
+		
+	//===================================================
+	
+
+	
+	if (strlen($new_agt) > $lvc_agent_max_length)
+	$new_agt = substr($new_agt, 0, $lvc_agent_max_length-4).' ...';
+	
+	return($new_agt);
+}
+
+?>
